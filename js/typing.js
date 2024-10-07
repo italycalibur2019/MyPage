921c588aaad9fe50335f6a0065d3eb73e1c1376c
dhr <920893925@qq.com>
2024-10-07 18:35:38 +0800

    Revert "fix(国际化):尝试通过整合打字机和国际化初始化的两个js文件，修复github page页面不能正确切换国际化的问题"
    This reverts commit bb398a69c81af24a85b5a416b3b609fa75413b99.

--- js/typing.js
+++ js/typing.js
@@ -6,57 +6,35 @@ let index = 0;
 
 let timer = undefined;
 
-let defaultLang = undefined;
+let lang = undefined;
 
 $(function () {
-    if (!defaultLang) {
-        defaultLang = 'en';
-    }
-    $("[i18n]").i18n({
-        defaultLang: defaultLang,
-        rootPath: "/MyPage",
-        //注意！要配置好语言文件的正确路径
-        filePath: "/i18n/",
-        filePrefix: "i18n_",
-        fileSuffix: "",
-        forever: true,
-        callback: function() {
-            console.log("i18n is ready.");
-        }
-    });
     autoTyping(element);
 });
 
 function autoTyping(e) {
+    if (!lang) {
+        lang = 'en';
+    }
     if (!text) {
         text = e.innerHTML;
     }else {
         /*切换为中文*/
         $(".chinese").click(function() {
-            defaultLang = 'zh';
-            $.getJSON('/MyPage/i18n/i18n_'+defaultLang+'.json', function(data) {
+            lang = 'zh';
+            $.getJSON('/MyPage/i18n/i18n_'+lang+'.json', function(data) {
                 const attr = element.getAttribute('i18n');
                 text = data[attr];
                 index = 0;
-            });
-            $("[i18n]").i18n({
-                defaultLang: "zh",
-                //注意！要配置好语言文件的正确路径
-                filePath: "/i18n/"
             });
         });
         /*切换为英文*/
         $(".english").click(function() {
-            defaultLang = 'en';
-            $.getJSON('/MyPage/i18n/i18n_'+defaultLang+'.json', function(data) {
+            lang = 'en';
+            $.getJSON('/MyPage/i18n/i18n_'+lang+'.json', function(data) {
                 const attr = element.getAttribute('i18n');
                 text = data[attr];
                 index = 0;
-            });
-            $("[i18n]").i18n({
-                defaultLang: "en",
-                //注意！要配置好语言文件的正确路径
-                filePath: "/i18n/"
             });
         });
     }
